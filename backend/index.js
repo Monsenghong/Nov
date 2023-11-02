10398be128070ed3339d58059854fee48b1c4119
Monsenghong <senghong601@gmail.com>
2023-11-02 21:49:56 +0700

    sixth

--- backend/index.js
+++ backend/index.js
@@ -6,6 +6,54 @@ import mongoose from 'mongoose'
 import authRouter from './routes/auth.js'
 
 const app = express()
+app.set('view engine', 'ejs')
+
+const posts = [
+    {
+      id: 1,
+      author: 'John',
+      title: 'Templating with EJS',
+      body: 'Blog post number 1'
+    },
+    {
+      id: 2,
+      author: 'Drake',
+      title: 'Express: Starting from the Bottom',
+      body: 'Blog post number 2'
+    },
+    {
+      id: 3,
+      author: 'Emma',
+      title: 'Streams',
+      body: 'Blog post number 3'
+    },
+    {
+      id: 4,
+      author: 'Cody',
+      title: 'Events',
+      body: 'Blog post number 4'
+    }
+  ]
+  // set the view engine to ejs
+  
+  // blog home page
+  app.get('/', (req, res) => {
+    // render `home.ejs` with the list of posts
+    res.render('home', { posts: posts })
+  })
+  // blog post
+  app.get('/post/:id', (req, res) => {
+    // find the post in the `posts` array
+    const post = posts.filter((post) => {
+      return post.id == req.params.id
+    })[0]
+    // render the `post.ejs` template with the post content
+    res.render('post', {
+      author: post.author,
+      title: post.title,
+      body: post.body
+    })
+  })
 
 dotenv.config()
 app.use(express.json())
@@ -30,10 +78,13 @@ app.use((err,req,res,next) => {
     res.status(err.status || 500).json({error:err.message})
 })
 
+
+
 app.listen(8080,() => {
 
     try {
         connect()
+       
         console.log("Server is running...")
 
         if(process.env.NODE_ENV == 'production'){
